938e044957845a9086113aebfff0ff2d3bb070e2
Ross Harper <ross@rossharper.net>
2016-02-24 21:59:58 +0000

    test dead cells are dead

--- test/LifeTest.kt
+++ test/LifeTest.kt
@@ -8,4 +8,11 @@ class LifeTest {
         assertTrue(life.isCellAlive(Cell(1, 0)))
         assertTrue(life.isCellAlive(Cell(0, 1)))
     }
+
+    @Test fun thatInitialDeadCellsAreDead() {
+        val life = Life(setOf(Cell(1, 0), Cell(0, 1)))
+
+        assertFalse(life.isCellAlive(Cell(0, 0)))
+        assertFalse(life.isCellAlive(Cell(1, 1)))
+    }
 }
