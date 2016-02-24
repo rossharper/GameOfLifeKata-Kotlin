f798389b97cc0dd7a9ba6ee29ed85d085352c829
Ross Harper <ross@rossharper.net>
2016-02-24 22:15:31 +0000

    test live cell with < 2 neighbouroonies will die

--- src/Life.kt
+++ src/Life.kt
@@ -1,5 +1,33 @@
+import java.util.*
+
 class Life(val initialLivingCells: Set<Cell>) {
     fun isCellAlive(cell: Cell): Boolean {
         return initialLivingCells.contains(cell)
+    }
+
+    fun evolve(): Life {
+        return Life(initialLivingCells.subtract(cellsThatShouldDie()))
+    }
+
+    private fun cellsThatShouldDie(): Iterable<Cell> {
+        return initialLivingCells.filter {
+            livingNeighboursOf(it).count() < 2
+        }
+    }
+
+    private fun livingNeighboursOf(cell: Cell): Iterable<Cell> {
+        return neighboursOf(cell).filter { isCellAlive(it) }
+    }
+
+    private fun neighboursOf(cell: Cell): Iterable<Cell> {
+        var neighbours : MutableSet<Cell> = hashSetOf()
+        for(x in -1..1) {
+            for(y in -1..1) {
+                if(x != 0 || y != 0) {
+                    neighbours.add(Cell(cell.x + x, cell.y + y))
+                }
+            }
+        }
+        return neighbours
     }
 }
--- test/LifeTest.kt
+++ test/LifeTest.kt
@@ -2,17 +2,26 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class LifeTest {
-    @Test fun thatInitialLivingCellsAreAlive() {
+    @Test fun thatInitialLivingCellsAreAliveBeforeEvolution() {
         val life = Life(setOf(Cell(1, 0), Cell(0, 1)))
 
         assertTrue(life.isCellAlive(Cell(1, 0)))
         assertTrue(life.isCellAlive(Cell(0, 1)))
     }
 
-    @Test fun thatInitialDeadCellsAreDead() {
+    @Test fun thatInitialDeadCellsAreDeadBeforeEvolution() {
         val life = Life(setOf(Cell(1, 0), Cell(0, 1)))
 
         assertFalse(life.isCellAlive(Cell(0, 0)))
         assertFalse(life.isCellAlive(Cell(1, 1)))
+    }
+
+    @Test fun thatLiveCellWithFewerThanTwoNeighboursShouldDie() {
+        var life = Life(setOf(Cell(1, 0), Cell(0, 1)))
+
+        life = life.evolve()
+
+        assertFalse(life.isCellAlive(Cell(1, 0)))
+        assertFalse(life.isCellAlive(Cell(0, 1)))
     }
 }
