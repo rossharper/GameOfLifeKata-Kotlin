256c499238c9c31ff355765f793dba661b3e1666
Ross Harper <ross@rossharper.net>
2016-02-24 22:35:21 +0000

    refactored to single-expression functions

--- src/Life.kt
+++ src/Life.kt
@@ -1,30 +1,19 @@
 
 class Life(val livingCells: Set<Cell>) {
-    fun isCellAlive(cell: Cell): Boolean {
-        return livingCells.contains(cell)
-    }
+    fun isCellAlive(cell: Cell): Boolean = livingCells.contains(cell)
 
-    fun evolve(): Life {
-        return Life(livingCells.subtract(cellsThatShouldDie()).union(cellsThatShouldBeBorn()))
-    }
+    fun evolve(): Life = Life(livingCells.subtract(cellsThatShouldDie()).union(cellsThatShouldBeBorn()))
 
-    private fun cellsThatShouldBeBorn(): Iterable<Cell> {
-        return livingCells.flatMap { deadNeighboursOf(it) }.filter { livingNeighboursOf(it).count() == 3 }.toSet()
-    }
+    private fun cellsThatShouldBeBorn(): Iterable<Cell>
+            = livingCells.flatMap { deadNeighboursOf(it) }.filter { livingNeighboursOf(it).count() == 3 }.toSet()
 
-    private fun cellsThatShouldDie(): Iterable<Cell> {
-        return livingCells.filter {
-            livingNeighboursOf(it).count() !in 2..3
-        }
-    }
+    private fun cellsThatShouldDie(): Iterable<Cell>
+            = livingCells.filter { livingNeighboursOf(it).count() !in 2..3 }
 
-    private fun livingNeighboursOf(cell: Cell): Iterable<Cell> {
-        return neighboursOf(cell).filter { isCellAlive(it) }
-    }
+    private fun livingNeighboursOf(cell: Cell): Iterable<Cell>
+            = neighboursOf(cell).filter { isCellAlive(it) }
 
-    private fun deadNeighboursOf(cell: Cell): Iterable<Cell> {
-        return neighboursOf(cell).filter { !isCellAlive(it) }
-    }
+    private fun deadNeighboursOf(cell: Cell): Iterable<Cell> = neighboursOf(cell).filter { !isCellAlive(it) }
 
     private fun neighboursOf(cell: Cell): Iterable<Cell> {
         var neighbours : MutableSet<Cell> = hashSetOf()
